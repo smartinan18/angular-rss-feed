7fd98c6555a600cc780f8912280e7972571a4868
smartinan.qualigy <soporte@qualigy.com>
2018-10-11 17:25:35 +0200

    feat: now the RSS feed is an angularJS component

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-(function (angular) {
-    'use strict';
+angular
+    .module('RSSFeedApp', ['ngSanitize'])
+    .component('rssNews', {
+        bindings: {
+            xml: '@'
+        },
+        controller: FeedCtrl,
+        controllerAs: 'vm',
+        templateUrl: 'templates/rss-news/rss-news.html',
+        link: FeedCtrl
+    })
 
-    angular.module('RSSFeedApp', []);
-    angular.module('RSSFeedApp')
-        .controller("FeedCtrl", function ($http) {
-            var vm = this;
-            $http.get('xml/faro.xml').then(function (response) {
-                vm.xml = response.data
-                var x2js = new X2JS()
-                vm.items = x2js.xml_str2json(vm.xml)
-                console.log(vm.items)
-            })
-        });
-})(angular);
+function FeedCtrl($http) {
+    var vm = this
+
+    vm.$onInit = activate
+
+    function activate() {
+        $http.get(vm.xml).then(xmlToJson)
+    }
+
+    function xmlToJson(response) {
+        vm.xml = response.data
+        var x2js = new X2JS()
+        vm.items = x2js.xml_str2json(vm.xml)
+    }
+}    
